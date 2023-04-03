b41da4a7fcfd689906734240afa3dc4e22a4e8f7
SafurePanashe <Safurepanashe17@gmail.com>
2023-04-03 11:35:36 +0200

    git

--- JS/main.js
+++ JS/main.js
@@ -14,68 +14,6 @@ switcch.addEventListener('click',function(){
 
 
 
-$(document).ready(function(){
-
-  $(".regular").slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    centerMode: true,
-
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        
-      }
-    }, {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        dots: true,
-        infinite: true,
-        autoplay: true,
-
-      }
-    },  {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      }
-    }]
-  });
-
-});
-
-
-let para = document.querySelector('#para');
-let b = document.querySelector('#b');
-
-b.addEventListener('click', function(){
-  para.classList.toggle('show');
-})
-let para2 = document.querySelector('#para2');
-let c = document.querySelector('#c');
-
-c.addEventListener('click', function(){
-  para2.classList.toggle('show2');
-})
-let para3 = document.querySelector('#para3');
-let d = document.querySelector('#d');
-
-d.addEventListener('click', function(){
-  para3.classList.toggle('show3');
-})
 
 
 
